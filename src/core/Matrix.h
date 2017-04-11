85b54e0304b2c7dd6f7a47e94afc3e9db5e6e776
Dom <dom@idom.me>
2017-04-11 20:39:37 +0800

    Update license.

--- src/core/Matrix.h
+++ src/core/Matrix.h
@@ -2,7 +2,7 @@
 //
 //  The MIT License (MIT)
 //
-//  Copyright (c) 2017-present, Cyder.org
+//  Copyright (c) 2017-present, cyder.org
 //  All rights reserved.
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy of
